336facf880bf322de288f26a7d31c8ebf21ef2b6
Romain Moudurier <rmouduri@student.42.fr>
2020-11-19 16:31:06 +0100

    [FIX] substr, start higher than s strlen

--- ft_substr.c
+++ ft_substr.c
@@ -6,17 +6,25 @@
 /*   By: romain <rmouduri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 17:59:45 by romain            #+#    #+#             */
-/*   Updated: 2020/11/16 18:12:55 by romain           ###   ########.fr       */
+/*   Updated: 2020/11/19 16:28:36 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
 
+	if (start >= ft_strlen(s))
+	{
+		if ((sub = malloc(sizeof(char) * 1)) == NULL)
+			return (NULL);
+		sub[0] = 0;
+		return (sub);
+	}
 	if ((sub = malloc(sizeof(char) * (len + 1))) == NULL)
 		return (NULL);
 	i = -1;
